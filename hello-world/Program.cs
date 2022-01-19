ff39558837c9391a5838aeadfba8f13038c06d37
omarsaidi <saidiomar.saidi@gmail.com>
2022-01-19 12:55:23 +0100

    first commit

---
+++ hello-world/Program.cs
@@ -0,0 +1,37 @@
+using BooksApi.Services;
+using BooksApi.Models;
+using System.Configuration;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// requires using Microsoft.Extensions.Options
+builder.Services.Configure<BookstoreDatabaseSettings>(
+	Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+
+builder.Services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+	sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+builder.Services.AddSingleton<BookService>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
